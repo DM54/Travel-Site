7d8bc1df4f9a7a63e083d19cb0aeca4f1c0f3f7e
Dyana Monroy <dyana.monroy17@live.com>
2019-11-08 14:36:21 -0500

    woking on it

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,26 +1,83 @@
-var gulp = require('gulp'),
- watch = require('gulp-watch'),
- browserSync = require('browser-sync').create(); 
-
-
-gulp.task('watch', () =>{
-	browserSync.init({
-		server:{
-			baseDir: "app"
-		}
-	});
-	gulp.watch('./app/index.html')
-		.on('change', browserSync.reload);
+var gulp = require('gulp'), // needed by both
+    // watch
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
+    // styles
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins'),
+    hexrgba = require('postcss-hexrgba'),
+    // scripts
+    webpack = require('webpack');
  
-    gulp.watch('./app/assets/styles/**/*.css')
-		.on('change', gulp.series('cssInject'));
+// watch
+gulp.task('watch', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+ 
+  watch('./app/index.html', function() {
+    browserSync.reload();
+    // html();
+  }); // file on computer you want to watch, what happens when changes occur
+ 
+  watch('./app/assets/styles/**/*.css', function() {
+    styles();
+    cssInject();
+  });
+ 
+  watch('./app/assets/scripts/**/*.js', function() {
+    scripts();
+    scriptsRefresh();
+  });
+ 
+});
+ 
+gulp.task('cssInject', cssInject);
+function cssInject() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+};
+ 
+// styles
+gulp.task('styles', styles);
+function styles() {
+  console.log("Styles task ran.");
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function (errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+};
+ 
+//scripts
+gulp.task('scripts', scripts);
+function scripts() {
+  webpack(require('../../webpack.config.js'), function (err, stats) {
+    if (err) {
+      console.log(err.toString());
+    }
+ 
+    console.log("\n" + stats.toString() + "\n");
+  });
+};
+ 
+gulp.task('scriptsRefresh', scriptsRefresh);
+function scriptsRefresh() {
+  browserSync.reload();
+};
 
 
-	});
 
-gulp.task('cssInject', gulp.series('styles', function(){
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
-    
 
-}));
+
+
+
