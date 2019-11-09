c33d7c77cce56a6af2f0a7fb1cab36db4359c128
Dyana Monroy <dyana.monroy17@live.com>
2019-11-09 13:24:57 -0500

    fixed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll'; 
 import $ from 'jquery';
 import stickyHeader from './modules/stickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 //var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var StickyHeader = new stickyHeader();
+var modal = new Modal();
